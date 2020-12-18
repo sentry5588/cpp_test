10d12a5a4dfe763e8592b1f8cc48ff6003fe9d87
sentry5588 <sentry5588@gmx.com>
2020-12-18 15:28:44 -0500

    Added copy assignment operator overload

--- src/cpp_primer_s13_1_4_rule_of_3_constructors.cpp
+++ src/cpp_primer_s13_1_4_rule_of_3_constructors.cpp
@@ -5,10 +5,19 @@ class test_constructors {
         test_constructors(double f) {
             age = f;
         }
+        test_constructors() {
+            age = -1.0;
+            std::cout << "No parameter initialization, age = " << age << std::endl; 
+        }
         test_constructors(const test_constructors &orig) {
             age = orig.age;
             std::cout << "Here is a copy constructor, age = " << age << std::endl;
-        } 
+        }
+        test_constructors& operator=(const test_constructors & orig) {
+            age = orig.age;
+            std::cout << "Here is copy assignment, age = " << age << std::endl;
+            return *this;
+        }
         ~test_constructors() {
             std::cout << "This is a destructor action, age = " << age << std::endl;
         }
@@ -17,7 +26,7 @@ class test_constructors {
 
 int main(void) {
     test_constructors t1(5.0);
-    test_constructors t_copy_ini = t1;
-    test_constructors t_direct_ini(t1);
+    test_constructors t2;
+    t2 = t1;
     return 0;
 }
